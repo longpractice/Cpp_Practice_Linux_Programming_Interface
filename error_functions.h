e5ff7cd2bf1c1c0683b9aa15d8cd2eb26c753ddd
longpractice <30654729+longpractice@users.noreply.github.com>
2017-10-11 12:16:16 +0200

    Add usageErr in cpp style
    Add usageErr() func to print usage error and exit. It is rewritten in C++ style.

---
+++ error_functions.h
@@ -0,0 +1,43 @@
+/*
+ * error_functions.h
+ *
+ *  Created on: Oct 11, 2017
+ *      Author: YNG
+ */
+
+#ifndef ERROR_FUNCTIONS_H_
+#define ERROR_FUNCTIONS_H_
+
+#include <stdio.h>
+#include <cstdarg>
+#include <stdlib.h>
+#include <iostream>
+#include <stdexcept>
+
+//The C style function as indicated in the book
+/*
+static inline void usageErrC(const char* format, ...)
+{
+	va_list argList;
+	fflush(stdout);
+
+	fprintf(stderr, "Usage: ");
+	va_start(argList, format);
+	vfprintf(stderr, format, argList);
+	va_end(argList);
+
+	fflush(stderr);
+	exit(EXIT_FAILURE);
+}
+*/
+
+//the C++11 style
+template<typename... Args>
+static inline void usageErr(const char* format, Args... args)
+{
+	std::cerr << std::flush;
+	printf(format, args...);
+	std::cerr << std::endl;
+	std::cerr << std::flush;
+	throw std::runtime_error("cmd line args mistaken.");
+}
